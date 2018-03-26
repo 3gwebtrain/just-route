e47c14d25ffbfe5231719c3eaeff929f60f52115
3gwebtrain <3gwebtrain@gmail.com>
2018-03-26 18:08:44 +0530

    just routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CalcComponent } from './calc/calc.component'
+import { CalcComponent } from './calc/calc.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './notfound/notfound.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import {ROUTING} from "./app.routing";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalcComponent
+    CalcComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
